98f7a6a393f408bf755a3710798a91e4b751f628
Dominic Pfeifer <d.pfeifer@novum-gemueseabo.de>
2018-02-04 11:14:02 +0100

    Toast hinzugefügt zum anzeigen der Leben

--- app/src/main/java/dominicpfeifer/srl/MainActivity.java
+++ app/src/main/java/dominicpfeifer/srl/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import java.util.Locale;
 import java.util.Random;
 import android.os.Vibrator;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -115,6 +116,7 @@ public class MainActivity extends Activity  implements
         }else{
             text = "NO";
             leben--;
+            multiplikator = 0;
             txtLeben.setText("Leben: " + leben);
             v.vibrate(pattern2, -1);
             editTextErgebnis.setText("");
@@ -124,6 +126,13 @@ public class MainActivity extends Activity  implements
                 intent.putExtra("highscore",points);
                 startActivity(intent);
                 //neue activity mit highscore
+            }else{
+                Context context = getApplicationContext();
+                CharSequence text1 = "FALSCH! du hast noch 1 Versuch";
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text1, duration);
+                toast.show();
             }
         }
 
